5e6d2d9dc31d5acbc578bd313e064dca62b534ac
oxtbuilder <oxtbuilder@github.com>
2016-05-26 17:38:30 -0400

    Fix strings and add basic error handling
    Signed-off-by: oxtbuilder <oxtbuilder@github.com>

--- windows/openxt-windows/build/build.bat
+++ windows/openxt-windows/build/build.bat
@@ -2,17 +2,17 @@ set BUILDID=%1
 set BRANCH=%2
 set REPO=%3
 
-set CUSTOM="yes"
+set CUSTOM=yes
 
-if "%BRANCH%"=="None" set BRANCH="master"
-if "%REPO%"=="None"   set REPO="git://github.com/OpenXT"
+if "%BRANCH%"=="None" set BRANCH=master
+if "%REPO%"=="None"   set REPO=git://github.com/OpenXT
 
-if "%REPO%"=="git://github.com/OpenXT" set CUSTOM="no"
+if "%REPO%"=="git://github.com/OpenXT" set CUSTOM=no
 
 rm -rf openxt
-git clone %REPO%/openxt.git
+git clone %REPO%/openxt.git || goto :error
 cd openxt
-git checkout %BRANCH%
+git checkout %BRANCH% || goto :error
 cd windows
 
 sed -i "s/Put Your Company Name Here/OpenXT/g" config\sample-config.xml
@@ -25,3 +25,8 @@ echo %BUILDID% > BUILD_ID
 if "%CUSTOM%"=="no"  rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -a BUILD_ID sdk.zip win-tools.zip xctools-iso.zip xc-windows.zip builds@192.168.0.10:/home/builds/win/%BRANCH%/
 if "%CUSTOM%"=="no"  rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -a xc-wintools.iso builds@192.168.0.10:/home/builds/builds/%BRANCH%/windows-tools/%BUILDID%/
 if "%CUSTOM%"=="yes" rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -a * builds@192.168.0.10:/home/builds/builds/windows-custom/%BUILDID%/
+
+exit /b 0
+
+:error
+exit /b %errorlevel%
