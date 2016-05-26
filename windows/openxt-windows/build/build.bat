8dd6945ac36ae5b4d1bc246da564518ab9b80ee2
oxtbuilder <oxtbuilder@github.com>
2016-05-26 17:11:21 -0400

    Add custom build support
    Signed-off-by: oxtbuilder <oxtbuilder@github.com>

--- windows/openxt-windows/build/build.bat
+++ windows/openxt-windows/build/build.bat
@@ -1,18 +1,27 @@
 set BUILDID=%1
 set BRANCH=%2
+set REPO=%3
+
+set CUSTOM="yes"
+
+if "%BRANCH%"=="None" set BRANCH="master"
+if "%REPO%"=="None"   set REPO="git://github.com/OpenXT"
+
+if "%REPO%"=="git://github.com/OpenXT" set CUSTOM="no"
 
 rm -rf openxt
-git clone git://github.com/OpenXT/openxt.git
+git clone %REPO%/openxt.git
 cd openxt
 git checkout %BRANCH%
 cd windows
 
 sed -i "s/Put Your Company Name Here/OpenXT/g" config\sample-config.xml
 
-powershell .\winbuild-prepare.ps1 config=sample-config.xml build=%BUILDID% branch=%BRANCH% certname=developer developer=true
+powershell .\winbuild-prepare.ps1 config=sample-config.xml build=%BUILDID% giturl=%REPO% branch=%BRANCH% certname=developer developer=true
 powershell .\winbuild-all.ps1
 
 cd output
 echo %BUILDID% > BUILD_ID
-rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -a BUILD_ID sdk.zip win-tools.zip xctools-iso.zip xc-windows.zip builds@192.168.0.10:/home/builds/win/%BRANCH%/
-rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -a xc-wintools.iso builds@192.168.0.10:/home/builds/builds/%BRANCH%/windows-tools/%BUILDID%/
+if "%CUSTOM%"=="no"  rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -a BUILD_ID sdk.zip win-tools.zip xctools-iso.zip xc-windows.zip builds@192.168.0.10:/home/builds/win/%BRANCH%/
+if "%CUSTOM%"=="no"  rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -a xc-wintools.iso builds@192.168.0.10:/home/builds/builds/%BRANCH%/windows-tools/%BUILDID%/
+if "%CUSTOM%"=="yes" rsync --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -a * builds@192.168.0.10:/home/builds/builds/windows-custom/%BUILDID%/
